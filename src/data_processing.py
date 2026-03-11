220917a1fdae3e5b0add1b820dff8b07c4ee6908
siham2006el-cpu <siham2006el@gmail.com>
2026-03-11 11:16:04 +0100

    ...

---
+++ src/data_processing.py
@@ -0,0 +1,43 @@
+# (Data Understanding)
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+# charger le fichier
+df = pd.read_excel(r"C:\Users\J.P.M\Downloads\ObesityDataSetr.xlsx")
+df
+# afficher les premières lignes
+print(df.head())
+# vérifier les valeurs manquantes
+print(df.isnull().sum())
+# Vérifier les statistiques générales
+df.describe()
+# Vérifier les doublons
+df.duplicated().sum()
+# supprimer les lignes dupliquées
+df = df.drop_duplicates()
+df
+# réinitialiser l'index
+df = df.reset_index(drop=True)
+df
+# détecter colonnes catégorielles
+col_category = df.select_dtypes(include=['object']).columns
+
+# transformer en category
+df[col_category ] = df[col_category ].astype('category')
+
+# vérifier
+df.info()
+# yes/no variables
+yes_no_cols = ['family_history_with_overweight','FAVC','SMOKE','SCC']
+
+for col in yes_no_cols:
+    df[col] = df[col].str.lower()
+    df[col] = df[col].map({'yes':1,'no':0})
+df
+df['NObeyesdad'] = df['NObeyesdad'].astype('category').cat.codes
+df['Gender'] = df['Gender'].map({'Male':0, 'Female':1})
+df['CAEC'] = df['CAEC'].apply(lambda x: 3 if x == 'Always' else (2 if x == 'Frequently' else (1 if x == 'Sometimes' else 0)))
+df['CALC'] = df['CALC'].apply(lambda x: 3 if x == 'Always' else (2 if x == 'Frequently' else (1 if x == 'Sometimes' else 0)))
+df['MTRANS'] = df['MTRANS'].map({'Automobile':0, 'Bike':1, 'Motorbike':2, 'Public_Transportation':3, 'Walking':4})
+df  
